4a51efcd56d26890b7632a25640c31511384bf21
Isaac Turner <spacey_sooty@outlook.com>
2023-10-03 21:40:03 +0800

    remove boilerplate

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -1,13 +1,6 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 #pragma once
 
-#include <string>
-
 #include <frc/TimedRobot.h>
-#include <frc/smartdashboard/SendableChooser.h>
 
 class Robot : public frc::TimedRobot {
  public:
@@ -23,8 +16,5 @@ class Robot : public frc::TimedRobot {
   void TestPeriodic() override;
 
  private:
-  frc::SendableChooser<std::string> m_chooser;
-  const std::string kAutoNameDefault = "Default";
-  const std::string kAutoNameCustom = "My Auto";
-  std::string m_autoSelected;
+
 };
