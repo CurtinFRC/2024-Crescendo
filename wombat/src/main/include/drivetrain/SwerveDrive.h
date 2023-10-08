6ee68d174c168af2f5f8b5c54a8653aa4e12bc69
Isaac Turner <spacey_sooty@outlook.com>
2023-10-08 19:44:01 +0800

    comment out swerve ready for merge

--- wombat/src/main/include/drivetrain/SwerveDrive.h
+++ wombat/src/main/include/drivetrain/SwerveDrive.h
@@ -1,38 +1,38 @@
-#pragma once
-
-#include "behaviour/Behaviour.h"
-#include "subsystems/Subsystem.h"
-#include "utils/PID.h"
-
-namespace wom {
-    enum ModuleName {
-        FrontLeft,
-        FrontRight,
-        BackLeft,
-        BackRight,
-    };
-
-    struct SwerveModuleConfig {};
-
-    enum SwerveModuleState {
-        kIdle,
-        kCalibration,
-        kPID,
-    };
-
-    class SwerveModule : public wom::Subsystem<SwerveModuleConfig, SwerveModuleState>, public behaviour::HasBehaviour {
-      public:
-          SwerveModule(wom::Subsystem<SwerveModuleConfig, SwerveModuleState> _, wom::ModuleName name, wom::SwerveModuleConfig config, wom::SwerveModuleState state);
-          ~SwerveModule();
-
-          void OnStart() override;
-          void OnUpdate(units::second_t dt) override;
-
-      protected:
-
-      private:
-          wom::ModuleName _name;
-
-    };
-
-};
+/* #pragma once */
+/**/
+/* #include "behaviour/Behaviour.h" */
+/* #include "subsystems/Subsystem.h" */
+/* #include "utils/PID.h" */
+/**/
+/* namespace wom { */
+/*     enum ModuleName { */
+/*         FrontLeft, */
+/*         FrontRight, */
+/*         BackLeft, */
+/*         BackRight, */
+/*     }; */
+/**/
+/*     struct SwerveModuleConfig {}; */
+/**/
+/*     enum SwerveModuleState { */
+/*         kIdle, */
+/*         kCalibration, */
+/*         kPID, */
+/*     }; */
+/**/
+/*     class SwerveModule : public wom::Subsystem<SwerveModuleConfig, SwerveModuleState>, public behaviour::HasBehaviour { */
+/*       public: */
+/*           SwerveModule(wom::Subsystem<SwerveModuleConfig, SwerveModuleState> _, wom::ModuleName name, wom::SwerveModuleConfig config, wom::SwerveModuleState state); */
+/*           ~SwerveModule(); */
+/**/
+/*           void OnStart() override; */
+/*           void OnUpdate(units::second_t dt) override; */
+/**/
+/*       protected: */
+/**/
+/*       private: */
+/*           wom::ModuleName _name; */
+/**/
+/*     }; */
+/**/
+/* }; */
