4dc4d7b7798b74cf4f802668b745b856c72aaf51
Isaac Turner <spacey_sooty@outlook.com>
2023-10-14 16:49:14 +0800

    add wombat.h

---
+++ wombat/src/main/include/Wombat.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "utils/PID.h"
+#include "utils/Util.h"
+#include "utils/Encoder.h"
+#include "utils/Gearbox.h"
+#include "utils/RobotStartup.h"
+#include "utils/VoltageController.h"
+
+#include "subsystems/Arm.h"
+#include "subsystems/Shooter.h"
+#include "subsystems/Elevator.h"
+
+#include "drivetrain/Drivetrain.h"
+#include "drivetrain/SwerveDrive.h"
+
+#include "behaviour/Behaviour.h"
+#include "behaviour/HasBehaviour.h"
+#include "behaviour/BehaviourScheduler.h"
+
+using namespace wom;
+using namespace behaviour;
+
