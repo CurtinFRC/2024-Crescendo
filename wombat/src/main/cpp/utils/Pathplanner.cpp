949600ef1fa4ecfb9e6e21f211498a546fe549bb
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-11-05 13:46:35 +0800

    Added pathweaver to wombat

--- wombat/src/main/cpp/utils/Pathplanner.cpp
+++ wombat/src/main/cpp/utils/Pathplanner.cpp
@@ -1,2 +1,10 @@
 #include "utils/Pathplanner.h"
 
+using namespace wom;
+
+utils::Pathplanner::Pathplanner() {};
+
+frc::Trajectory utils::Pathplanner::getTrajectory(std::string_view path) {
+    fs::path path_location = deploy_directory / path;
+    return frc::TrajectoryUtil::FromPathweaverJson(path_location.string());
+}
--- wombat/src/main/include/utils/Pathplanner.h
+++ wombat/src/main/include/utils/Pathplanner.h
@@ -1,51 +1,22 @@
 #pragma once
 
-#include <units/voltage.h>
-#include <units/velocity.h>
-#include <units/acceleration.h>
-#include <units/angular_velocity.h>
-#include <units/angular_acceleration.h>
-#include <units/length.h>
+#include "utils/Util.h"
+
+#include <frc/Filesystem.h>
+#include <frc/trajectory/TrajectoryUtil.h>
+#include <wpi/fs.h>
+
 
 namespace wom {
 namespace utils {
     class Pathplanner {
-     public:
-      Pathplanner(double ks, double kv, double ka, double kp, double trackwidth)
-          : _ks(ks), _kv(kv), _ka(ka), _kp(kp), _trackwidth(trackwidth) {}
+    public:
+        Pathplanner();
 
-      double GetKs() const { return _ks; }
-      double GetKv() const { return _kv; }
-      double GetKa() const { return _ka; }
-      double GetPDriveVel() const { return _kPDriveVel; }
-      double GetTrackWidth() const { return _kTrackwidth; }
+        frc::Trajectory getTrajectory(std::string_view path);
 
-
-
-     private:
-      units::volt _ks;
-      auto _kv;
-      auto _ka;
-      double _kPDriveVel;
-      units::meter _kTrackwidth;
-
-      units::meters_per_second_t _kMaxSpeed;
-      units::meters_per_second_squared_t _kMaxAcceleration;
-
-      auto kRamseteB;
-      auto kRamseteZeta;
-
-      frc::ADXRS450_Gyro m_gyro;
+    private:
+        fs::path deploy_directory = frc::filesystem::GetDeployDirectory();
     };
 }
 }
-// TODO: Tune the values to our drivebase
-
-constexpr auto ks = 0.22_V;
-constexpr auto kv = 1.98 * 1_V * 1_s / 1_m;
-constexpr auto ka = 0.2 * 1_V * 1_s * 1_s / 1_m;
-
-constexpr double kPDriveVel = 8.5;
-
-constexpr auto kTrackwidth = 0.69_m;
-extern const frc::DifferentialDriveKinematics kDriveKinematics;
