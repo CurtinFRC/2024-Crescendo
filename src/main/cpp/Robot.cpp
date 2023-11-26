7899a02c7e7c8b2819c52ebb5b803c59be3b36a9
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-11-26 16:38:28 +0800

    Added robot simulation code

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,7 +1,18 @@
 #include "Robot.h"
 
-void Robot::RobotInit() {}
-void Robot::RobotPeriodic() {}
+void Robot::RobotInit() {
+    m_chooser.SetDefaultOption("Default Auto", "Default Auto");
+
+    frc::SmartDashboard::PutData("Auto Selector", &m_chooser);
+
+    frc::SmartDashboard::PutData("Field", &m_field);
+
+}
+void Robot::RobotPeriodic() {
+    //m_field.SetRobotPose(m_odometry.GetPose());
+
+    
+}
 
 void Robot::AutonomousInit() {}
 void Robot::AutonomousPeriodic() {}
@@ -14,3 +25,8 @@ void Robot::DisabledPeriodic() {}
 
 void Robot::TestInit() {}
 void Robot::TestPeriodic() {}
+
+void Robot::SimulationInit() {
+
+}
+void Robot::SimulationPeriodic() {}
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -1,6 +1,15 @@
 #pragma once
 
 #include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include "frc/smartdashboard/SendableChooser.h"
+#include <frc/smartdashboard/Field2d.h>
+
+#include <frc/Encoder.h>
+#include <frc/simulation/EncoderSim.h>
+
+#include <frc/simulation/DifferentialDrivetrainSim.h>
+
 
 class Robot : public frc::TimedRobot {
  public:
@@ -14,7 +23,26 @@ class Robot : public frc::TimedRobot {
   void DisabledPeriodic() override;
   void TestInit() override;
   void TestPeriodic() override;
+  void SimulationInit() override;
+  void SimulationPeriodic() override;
 
  private:
+    frc::SendableChooser<std::string> m_chooser;
 
+    frc::Field2d m_field;
+
+    frc::sim::DifferentialDrivetrainSim m_driveSim{
+            frc::DCMotor::NEO(2), // 2 NEO motors on each side of the drivetrain.
+            7.29,               // 7.29:1 gearing reduction.
+            7.5_kg_sq_m,        // MOI of 7.5 kg m^2 (from CAD model).
+            60_kg,              // The mass of the robot is 60 kg.
+            3_in,               // The robot uses 3" radius wheels.
+            0.7112_m,           // The track width is 0.7112 meters.
+
+            // The standard deviations for measurement noise:
+            // x and y:          0.001 m
+            // heading:          0.001 rad
+            // l and r velocity: 0.1   m/s
+            // l and r position: 0.005 m
+            {0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005}};
 };
