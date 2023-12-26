91dd152d757645b0453d104dea6520bd2d1931ff
Isaac Turner <spacey_sooty@outlook.com>
2023-12-26 17:06:13 +0000

    [wombat] fix warnings

--- wombat/src/main/include/utils/Encoder.h
+++ wombat/src/main/include/utils/Encoder.h
@@ -13,7 +13,7 @@
 namespace wom {
   class Encoder {
    public:
-    Encoder(double encoderTicksPerRotation, double reduction, int type) : _encoderTicksPerRotation(encoderTicksPerRotation), _reduction(reduction), _type(type) {};
+    Encoder(double encoderTicksPerRotation, double reduction = 1.0, int type) : _reduction(reduction), _encoderTicksPerRotation(encoderTicksPerRotation), _type(type) {};
     virtual double    GetEncoderRawTicks() const = 0;
     virtual double    GetEncoderTickVelocity() const = 0;  // ticks/s
     virtual void      ZeroEncoder();
@@ -31,7 +31,7 @@ namespace wom {
     units::radians_per_second_t GetEncoderAngularVelocity();   // rad/s
 
     int encoderType = 0;
-    double _reduction = 1.0;
+    double _reduction;
    private:
     double _encoderTicksPerRotation;
     units::radian_t _offset = 0_rad;
@@ -75,29 +75,29 @@ namespace wom {
   };
 
   class TalonSRXEncoder : public Encoder {
-   public: 
+   public:
     TalonSRXEncoder(ctre::phoenix::motorcontrol::can::TalonSRX *controller, double ticksPerRotation, double reduction = 1);
-   
+
     double GetEncoderRawTicks() const override;
     double GetEncoderTickVelocity() const override;
 
-   private: 
+   private:
     ctre::phoenix::motorcontrol::can::TalonSRX *_controller;
   };
 
   class DutyCycleEncoder : public Encoder {
-   public: 
+   public:
     DutyCycleEncoder(int channel, double ticksPerRotation = 1, double reduction = 1);
 
     double GetEncoderRawTicks() const override;
     double GetEncoderTickVelocity() const override;
 
-   private: 
+   private:
     frc::DutyCycleEncoder _dutyCycleEncoder;
   };
 
   class CanEncoder : public Encoder {
-    public: 
+    public:
       CanEncoder(int deviceNumber, double ticksPerRotation = 4095, double reduction = 1);
 
       double GetEncoderRawTicks() const override;
@@ -106,7 +106,7 @@ namespace wom {
 
       const double constantValue = 0.0;
 
-    private: 
+    private:
       CANCoder *_canEncoder;
   };
 }  // namespace wom
