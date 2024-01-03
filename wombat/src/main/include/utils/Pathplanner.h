f590457daee71bd4f1e3fc1c10c789e194417216
Isaac Turner <spacey_sooty@outlook.com>
2024-01-03 10:39:00 +0800

    ran wpiformat

--- wombat/src/main/include/utils/Pathplanner.h
+++ wombat/src/main/include/utils/Pathplanner.h
@@ -1,22 +1,25 @@
-#pragma once
+// Copyright (c) 2023-2024 CurtinFRC
+// Open Source Software, you can modify it according to the terms
+// of the MIT License at the root of this project
 
-#include "utils/Util.h"
+#pragma once
 
 #include <frc/Filesystem.h>
 #include <frc/trajectory/TrajectoryUtil.h>
 #include <wpi/fs.h>
 
+#include "utils/Util.h"
 
 namespace wom {
 namespace utils {
-    class Pathplanner {
-    public:
-        Pathplanner();
+class Pathplanner {
+ public:
+  Pathplanner();
 
-        frc::Trajectory getTrajectory(std::string_view path);
+  frc::Trajectory getTrajectory(std::string_view path);
 
-    private:
-        fs::path deploy_directory = frc::filesystem::GetDeployDirectory();
-    };
-}
-}
+ private:
+  fs::path deploy_directory = frc::filesystem::GetDeployDirectory();
+};
+}  // namespace utils
+}  // namespace wom
