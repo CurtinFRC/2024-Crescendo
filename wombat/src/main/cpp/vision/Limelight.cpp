385157276b7094108cbbbeebbd89738832c8f98e
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2024-01-11 12:56:35 +0800

    Updated limelight to use enums

--- wombat/src/main/cpp/vision/Limelight.cpp
+++ wombat/src/main/cpp/vision/Limelight.cpp
@@ -24,9 +24,136 @@ std::pair<double, double> wom::vision::Limelight::GetOffset() {
   return offset;
 }
 
-std::vector<double> wom::vision::Limelight::GetAprilTagData(
-    std::string dataName) {
+std::vector<double> wom::vision::Limelight::GetAprilTagData(LimelightAprilTagData dataType) {
+  std::string dataName;
+
+  switch (dataType) {
+    case LimelightAprilTagData::kBotpose:
+      dataName = "botpose";
+      break;
+
+    case LimelightAprilTagData::kBotpose_wpiblue:
+      dataName = "botpose_wpiblue";
+      break;
+
+    case LimelightAprilTagData::kBotpose_wpired:
+      dataName = "botpose_wpired";
+      break;
+
+    case LimelightAprilTagData::kCamerapose_targetspace:
+      dataName = "camerapose_targetspace";
+      break;
+
+    case LimelightAprilTagData::kTargetpose_cameraspace:
+      dataName = "targetpose_cameraspace";
+      break;
+
+    case LimelightAprilTagData::kTargetpose_robotspace:
+      dataName = "targetpose_robotspace";
+      break;
+
+    case LimelightAprilTagData::kBotpose_targetspace:
+      dataName = "botpose_targetspace	";
+      break;
+
+    case LimelightAprilTagData::kCamerapose_robotspace:
+      dataName = "camerapose_robotspace";
+      break;
+
+    case LimelightAprilTagData::kTid:
+      dataName = "tid";
+      break;
+  }
+
   return table->GetNumberArray(dataName, std::vector<double>(6));
+}
+
+double wom::vision::Limelight::GetTargetingData(LimelightTargetingData dataType, double defaultValue) {
+  std::string dataName;
+
+  switch (dataType) {
+    case LimelightTargetingData::kTv:
+      dataName = "tv";
+      break;
+
+    case LimelightTargetingData::kTx:
+      dataName = "tx";
+      break;
+
+    case LimelightTargetingData::kTy:
+      dataName = "ty";
+      break;
+
+    case LimelightTargetingData::kTa:
+      dataName = "ta";
+      break;
+
+    case LimelightTargetingData::kTl:
+      dataName = "tl";
+      break;
+
+    case LimelightTargetingData::kCl:
+      dataName = "cl";
+      break;
+
+    case LimelightTargetingData::kTshort:
+      dataName = "tshort";
+      break;
+
+    case LimelightTargetingData::kTlong:
+      dataName = "tlong";
+      break;
+
+    case LimelightTargetingData::kThor:
+      dataName = "thor";
+      break;
+
+    case LimelightTargetingData::kTvert:
+      dataName = "tvert";
+      break;
+
+    case LimelightTargetingData::kGetpipe:
+      dataName = "getpipe";
+      break;
+
+    case LimelightTargetingData::kJson:
+      dataName = "json";
+      break;
+
+    case LimelightTargetingData::kClass:
+      dataName = "class";
+      break;
+
+    case LimelightTargetingData::kTc:
+      dataName = "tc";
+      break;
+  }
+  
+  return table->GetNumber(dataName, defaultValue);
+}
+
+void wom::vision::Limelight::SetLEDMode(LimelightLEDMode mode) {
+  table->PutNumber("ledMode", static_cast<double>(mode));
+}
+
+void wom::vision::Limelight::SetCamMode(LimelightCamMode mode) {
+  table->PutNumber("camMode", static_cast<double>(mode));
+}
+
+void wom::vision::Limelight::SetPipeline(LimelightPipeline pipeline) {
+  table->PutNumber("pipeline", static_cast<double>(pipeline));
+}
+
+void wom::vision::Limelight::SetStreamMode(LimelightStreamMode mode) {
+  table->PutNumber("stream", static_cast<double>(mode));
+}
+
+void wom::vision::Limelight::SetSnapshotMode(LimelightSnapshotMode mode) {
+  table->PutNumber("snapshot", static_cast<double>(mode));
+}
+
+void wom::vision::Limelight::SetCrop(std::array<double, 4> crop) {
+  table->PutNumberArray("camtran", crop);
 }
 
 units::meters_per_second_t wom::vision::Limelight::GetSpeed(
@@ -42,7 +169,7 @@ units::meters_per_second_t wom::vision::Limelight::GetSpeed(
 }
 
 frc::Pose3d wom::vision::Limelight::GetPose() {
-  std::vector<double> pose = GetAprilTagData("botpose");
+  std::vector<double> pose = GetAprilTagData(LimelightAprilTagData::kBotpose);
   return frc::Pose3d(
       pose[1] * 1_m, 1_m * pose[2], 1_m * pose[3],
       frc::Rotation3d(1_deg * (pose[4]), 1_deg * (pose[5]), 1_deg * (pose[6])));
--- wombat/src/main/include/vision/Limelight.h
+++ wombat/src/main/include/vision/Limelight.h
@@ -27,7 +27,73 @@
 
 namespace wom {
 namespace vision {
-class Limelight : public behaviour::HasBehaviour {
+
+  enum class LimelightLEDMode {
+    kPipelineDefault = 0,
+    kForceOff = 1,
+    kForceBlink = 2,
+    kForceOn = 3
+  };
+
+  enum class LimelightCamMode {
+    kVisionProcessor = 0,
+    kDriverCamera = 1
+  };
+
+  enum class LimelightStreamMode {
+    kStandard = 0,
+    kPiPMain = 1,
+    kPiPSecondary = 2
+  };
+
+  enum class LimelightSnapshotMode {
+    kReset = 0,
+    kSingle = 1
+  };
+
+  enum class LimelightPipeline {
+    kPipeline0 = 0,
+    kPipeline1 = 1,
+    kPipeline2 = 2,
+    kPipeline3 = 3,
+    kPipeline4 = 4,
+    kPipeline5 = 5,
+    kPipeline6 = 6,
+    kPipeline7 = 7,
+    kPipeline8 = 8,
+    kPipeline9 = 9
+  };
+
+  enum class LimelightTargetingData {
+    kTv = 0,
+    kTx = 1,
+    kTy = 2,
+    kTa = 3,
+    kTl = 4,
+    kCl = 5,
+    kTshort = 6,
+    kTlong = 7,
+    kThor = 8,
+    kTvert = 9,
+    kGetpipe = 10,
+    kJson = 11,
+    kClass = 12,
+    kTc = 13,
+  };
+
+  enum class LimelightAprilTagData {
+    kBotpose = 0,
+    kBotpose_wpiblue = 1,
+    kBotpose_wpired = 2,
+    kCamerapose_targetspace = 3,
+    kTargetpose_cameraspace = 4,
+    kTargetpose_robotspace = 5,
+    kBotpose_targetspace = 6,
+    kCamerapose_robotspace = 7,
+    kTid = 8
+  };
+
+class Limelight {
  public:
   explicit Limelight(std::string limelightName);
 
@@ -38,7 +104,14 @@ class Limelight : public behaviour::HasBehaviour {
 
   std::pair<double, double> GetOffset();
 
-  std::vector<double> GetAprilTagData(std::string dataName);
+  std::vector<double> GetAprilTagData(LimelightAprilTagData dataType);
+  double GetTargetingData(LimelightTargetingData dataType, double defaultValue = 0.0);
+  void SetLEDMode(LimelightLEDMode mode);
+  void SetCamMode(LimelightCamMode mode);
+  void SetPipeline(LimelightPipeline pipeline);
+  void SetStreamMode(LimelightStreamMode mode);
+  void SetSnapshotMode(LimelightSnapshotMode mode);
+  void SetCrop(std::array<double, 4> crop);
 
   void OnUpdate(units::time::second_t dt);
   void OnStart();
