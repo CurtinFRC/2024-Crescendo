7e9b5f95a6dab841acbad5c282d40746cb69e621
Sean Chan <sean.chan3@student.education.wa.edu.au>
2024-01-28 11:50:38 +0800

    Added auto to intake

--- src/main/cpp/IntakeBehaviour.cpp
+++ src/main/cpp/IntakeBehaviour.cpp
@@ -6,8 +6,7 @@
 
 #include <frc/XboxController.h>
 
-IntakeManualControl::IntakeManualControl(Intake* intake, frc::XboxController& codriver)
-    : _intake(intake), _codriver(codriver) {
+IntakeManualControl::IntakeManualControl(Intake* intake, frc::XboxController& codriver) : _intake(intake), _codriver(codriver) {
   Controls(intake);
 }
 
@@ -19,38 +18,38 @@ void IntakeManualControl::OnTick(units::second_t dt) {
       _rawControl = true;
     }
   }
-  if (_codriver.GetRightTriggerAxis() > 0.1) {
-    _intake->setRaw(_codriver.GetRightTriggerAxis() * 10_V);
-  } else if (_codriver.GetLeftTriggerAxis() > 0.1) {
-    _intake->setRaw(_codriver.GetLeftTriggerAxis() * -10_V);
-  } else if (_rawVoltage != 0_V) {
-    if (_intake->GetConfig().intakeSensor->Get() == 1) {
-      _intake->setRaw(0_V);
+
+  if (_rawControl) {
+    if (_codriver.GetRightTriggerAxis() > 0.1) {
+      _intake->setRaw(_codriver.GetRightTriggerAxis() * 10_V);
+    } else if (_codriver.GetLeftTriggerAxis() > 0.1) {
+      _intake->setRaw(_codriver.GetLeftTriggerAxis() * -10_V);
+    } else if (_rawVoltage != 0_V) {
+      if (_intake->GetConfig().intakeSensor->Get() == 1) {
+        _intake->setRaw(0_V);
+      } else {
+        _intake->setRaw(10_V);
+      } 
     } else {
       _intake->setRaw(0_V);
-    } 
-  } else {
-    _intake->setRaw(0_V);
-  }
+    }
     _intake->setState(IntakeState::kRaw);
+
+  } else {
+     if (_intake->GetConfig().intakeSensor->Get() == 1) {
+        _intake->setState(IntakeState::kHold);
+      } else if (_intake->GetConfig().intakeSensor->Get() == 0) {
+        if (_codriver.GetRightTriggerAxis() > 0.1) {
+          _intake->setState(IntakeState::kIntake);
+        } else {
+          _intake->setState(IntakeState::kIdle);
+        }
+      }
+    }
+  }
+
+IntakeAutoControl::IntakeAutoControl(Intake* intake) : _intake(intake) {
+  Controls(intake);
 }
 
-  // } else {
-  //   if (_codriver.GetYButtonPressed()) {
-  //     _intake->setState(IntakeState::kIntake);
-  //   }
-  //   if (_codriver.GetAButtonPressed()) {
-  //     _intake->setState(IntakeState::kPass);
-  //   }
-  // }
-
-
-// IntakeAutoControl::IntakeAutoControl(Intake* intake) : _intake(intake) {}
-
-void IntakeAutoControl::OnTick(units::second_t dt) {
-  if (_intake->GetConfig().intakeSensor->Get() == 1) {
-    _intake->setState(IntakeState::kPass);
-  } /*else if (_intake->GetConfig().magSensor->Get() == 0) {
-    _intake->setState(IntakeState::kIdle);
-  }*/
-}
+void IntakeAutoControl::OnTick(units::second_t dt) {}
