ae1a0fcd51e3c4b488d44f4f839a7f827fe7de73
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2024-01-29 12:40:30 +0800

    [wpiformat + add ctre_sim to gitignore]

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -148,7 +148,14 @@ void Robot::TeleopInit() {
 
 }
 
-void Robot::TeleopPeriodic() {}
+void Robot::TeleopPeriodic() {
+  // std::cout << "Current AprilTag: " << _vision->CurrentAprilTag() << std::endl;
+  // std::cout << "Current Target: " << _vision->TargetIsVisible(VisionTargetObjects::kNote) << std::endl;
+  std::cout << "Dist to target: " << _vision->GetDistanceToTarget(VisionTarget::kBlueAmp).first.value()
+            << std::endl;
+  std::cout << "Angle to target: " << _vision->GetDistanceToTarget(VisionTarget::kBlueAmp).second.value()
+            << std::endl;
+}
 
 void Robot::DisabledInit() {}
 void Robot::DisabledPeriodic() {}
@@ -159,9 +166,9 @@ void Robot::TestPeriodic() {}
 void Robot::SimulationInit() {
   std::string x = "[";
   std::string y = "[";
-  //_vision->GetDistanceToTarget(16);
-  for (int i = 1; i< 17; i++) {
-    for (int j = 0; j<17; j++) {
+  // _vision->GetDistanceToTarget(16);
+  for (int i = 1; i < 17; i++) {
+    for (int j = 0; j < 17; j++) {
       frc::Pose2d pose = _vision->AlignToTarget(i, units::meter_t{j * 0.1}, _swerveDrive);
       x += std::to_string(pose.X().value()) + ",";
       y += std::to_string(pose.Y().value()) + ",";
