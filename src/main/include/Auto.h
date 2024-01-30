2cb49082566428f88dc45ec3e6f4156456840853
SoulOfTheAssassin <oli.liu798@gmail.com>
2024-01-30 16:14:11 +0800

    Fixed some issues

--- src/main/include/Auto.h
+++ src/main/include/Auto.h
@@ -1,20 +1,23 @@
 
-// #pragma once
+#pragma once
 
-// #include "Wombat.h"
+#include "Wombat.h"
+#include "Intake.h"
+#include "Shooter.h"
+#include "AlphaArm.h"
 
-// namespace autos {
-//   std::shared_ptr<behaviour::Behaviour> AutoTest(Drivebase driveBase, Shooter shooter, Mag mag, Intake intake, Arm _arm);
+namespace autos {
+  std::shared_ptr<behaviour::Behaviour> AutoTest(wom::drivetrain::SwerveDrive Swervedrive, wom::subsystems::Shooter shooter, Intake intake, wom::subsystems::Arm _arm);
 
-//   // std::shared_ptr<behaviour::Behaviour> Taxi(Drivebase driveBase, Shooter shooter, Mag mag, Intake intake, Arm _arm);
+  // std::shared_ptr<behaviour::Behaviour> Taxi(SwerveDrive Swervedrive, Shooter shooter, Mag mag, Intake intake, Arm _arm);
 
-//   // std::shared_ptr<behaviour::Behaviour> QuadrupleClose(Drivebase driveBase, Shooter shooter, Mag mag, Intake intake, Arm _arm);
+  // std::shared_ptr<behaviour::Behaviour> QuadrupleClose(SwerveDrive Swervedrive, Shooter shooter, Mag mag, Intake intake, Arm _arm);
 
-//   // std::shared_ptr<behaviour::Behaviour> QuadrupleFar(Drivebase driveBase, Shooter shooter, Mag mag, Intake intake, Arm _arm);
+  // std::shared_ptr<behaviour::Behaviour> QuadrupleFar(SwerveDrive Swervedrive, Shooter shooter, Mag mag, Intake intake, Arm _arm);
 
-//   // std::shared_ptr<behaviour::Behaviour> QuadrupleCloseDoubleFar(Drivebase driveBase, Shooter shooter, Mag mag, Intake intake, Arm _arm);
+  // std::shared_ptr<behaviour::Behaviour> QuadrupleCloseDoubleFar(SwerveDrive Swervedrive, Shooter shooter, Mag mag, Intake intake, Arm _arm);
 
-//   // std::shared_ptr<behaviour::Behaviour> QuadrupleCloseSingleFar(Drivebase driveBase, Shooter shooter, Mag mag, Intake intake, Arm _arm);
-// }
+  // std::shared_ptr<behaviour::Behaviour> QuadrupleCloseSingleFar(SwerveDrive Swervedrive, Shooter shooter, Mag mag, Intake intake, Arm _arm);
+}
 
 
