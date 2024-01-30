d0cc0b03d45fe01173880656c33d1ae1ada74611
Isaac Turner <spacey_sooty@outlook.com>
2024-01-30 13:14:46 +0800

    merged with silly stuff

--- src/main/include/RobotMap.h
+++ src/main/include/RobotMap.h
@@ -18,100 +18,122 @@
 #include <ctre/phoenix6/TalonFX.hpp>
 
 #include "Wombat.h"
+#include "AlphaArm.h"
+#include "AlphaArmBehaviour.h"
 
 struct RobotMap {
   struct Controllers {
     frc::XboxController driver = frc::XboxController(0);
-    frc::XboxController coDriver = frc::XboxController(1);
+    frc::XboxController codriver = frc::XboxController(1);
     frc::XboxController testController = frc::XboxController(2);
   };
   Controllers controllers;
 
-  struct SwerveBase {
-    ctre::phoenix6::hardware::CANcoder frontLeftCancoder{19};
-    ctre::phoenix6::hardware::CANcoder frontRightCancoder{17};
-    ctre::phoenix6::hardware::CANcoder backLeftCancoder{16};
-    ctre::phoenix6::hardware::CANcoder backRightCancoder{18};
+  struct AlphaArmSystem {
+    rev::CANSparkMax alphaArmMotor{12, rev::CANSparkMax::MotorType::kBrushless};
+    rev::CANSparkMax wristMotor{15, rev::CANSparkMax::MotorType::kBrushless};
 
-    ctre::phoenix6::hardware::Pigeon2* gyro = new ctre::phoenix6::hardware::Pigeon2(20, "Drivebase");
+    wom::Gearbox alphaArmGearbox{&alphaArmMotor, nullptr, frc::DCMotor::NEO(1)};
+    wom::Gearbox wristGearbox{&wristMotor, nullptr, frc::DCMotor::NEO(1)};
+
+    AlphaArmConfig config{alphaArmGearbox, wristGearbox};
+  };
+  AlphaArmSystem alphaArmSystem;
+
+  struct SwerveBase {
+    ctre::phoenix6::hardware::CANcoder frontLeftCancoder{16, "Drivebase"};
+    ctre::phoenix6::hardware::CANcoder frontRightCancoder{18, "Drivebase"};
+    ctre::phoenix6::hardware::CANcoder backLeftCancoder{17, "Drivebase"};
+    ctre::phoenix6::hardware::CANcoder backRightCancoder{19, "Drivebase"};
+
+    ctre::phoenix6::hardware::Pigeon2* gyro =
+        new ctre::phoenix6::hardware::Pigeon2(20, "Drivebase");
     wpi::array<ctre::phoenix6::hardware::TalonFX*, 4> turnMotors{
-        new ctre::phoenix6::hardware::TalonFX(7, "Drivebase"),
-        new ctre::phoenix6::hardware::TalonFX(5, "Drivebase"),
-        new ctre::phoenix6::hardware::TalonFX(1, "Drivebase"),
-        new ctre::phoenix6::hardware::TalonFX(3, "Drivebase")};
+        new ctre::phoenix6::hardware::TalonFX(6, "Drivebase"),   // front left
+        new ctre::phoenix6::hardware::TalonFX(7, "Drivebase"),   // front right
+        new ctre::phoenix6::hardware::TalonFX(4, "Drivebase"),   // back left
+        new ctre::phoenix6::hardware::TalonFX(2, "Drivebase")};  // back right
     wpi::array<ctre::phoenix6::hardware::TalonFX*, 4> driveMotors{
-        new ctre::phoenix6::hardware::TalonFX(9, "Drivebase"),
-        new ctre::phoenix6::hardware::TalonFX(6, "Drivebase"),
-        new ctre::phoenix6::hardware::TalonFX(2, "Drivebase"),
-        new ctre::phoenix6::hardware::TalonFX(4, "Drivebase")};
+        new ctre::phoenix6::hardware::TalonFX(5, "Drivebase"),   // front left
+        new ctre::phoenix6::hardware::TalonFX(9, "Drivebase"),   // front right
+        new ctre::phoenix6::hardware::TalonFX(3, "Drivebase"),   // back left
+        new ctre::phoenix6::hardware::TalonFX(1, "Drivebase")};  // back right
 
     wpi::array<wom::SwerveModuleConfig, 4> moduleConfigs{
-        wom::SwerveModuleConfig{
+        wom::SwerveModuleConfig{ //CORRECT
             // front left module
-            frc::Translation2d(10.761_in, 9.455_in),
-            wom::Gearbox{driveMotors[0], new wom::TalonFXEncoder(driveMotors[0], 0.0445_m, 6.75),
-                         frc::DCMotor::Falcon500(1).WithReduction(6.75)},
-            wom::Gearbox{turnMotors[0], new wom::CanEncoder(19, 0.0445_m, 4096, 12.8),
+            frc::Translation2d(-10_in, 9_in),
+            wom::Gearbox{
+                driveMotors[0],
+                new wom::TalonFXEncoder(driveMotors[0], 0.0445_m, 6.75),
+                frc::DCMotor::Falcon500(1).WithReduction(6.75)},
+            wom::Gearbox{turnMotors[0],
+                         new wom::CanEncoder(16, 0.0445_m, 4096, 12.8),
                          frc::DCMotor::Falcon500(1).WithReduction(12.8)},
             &frontLeftCancoder, 4_in / 2},
-        wom::SwerveModuleConfig{
+        wom::SwerveModuleConfig{ //CORRECT
             // front right module
-            frc::Translation2d(10.761_in, -9.455_in),
-            wom::Gearbox{driveMotors[1], new wom::TalonFXEncoder(driveMotors[1], 0.0445_m, 6.75),
-                         frc::DCMotor::Falcon500(1).WithReduction(6.75)},
-            wom::Gearbox{turnMotors[1], new wom::CanEncoder(17, 0.0445_m, 4096, 12.8),
+            frc::Translation2d(10_in, 9_in),
+            wom::Gearbox{
+                driveMotors[1],
+                new wom::TalonFXEncoder(driveMotors[1], 0.0445_m, 6.75),
+                frc::DCMotor::Falcon500(1).WithReduction(6.75)},
+            wom::Gearbox{turnMotors[1],
+                         new wom::CanEncoder(18, 0.0445_m, 4096, 12.8),
                          frc::DCMotor::Falcon500(1).WithReduction(12.8)},
             &frontRightCancoder, 4_in / 2},
         wom::SwerveModuleConfig{
             // back left module
-            frc::Translation2d(-10.761_in, 9.455_in),
-            wom::Gearbox{driveMotors[2], new wom::TalonFXEncoder(driveMotors[2], 0.0445_m, 6.75),
-                         frc::DCMotor::Falcon500(1).WithReduction(6.75)},
-            wom::Gearbox{turnMotors[2], new wom::CanEncoder(16, 0.0445_m, 4096, 12.8),
+            frc::Translation2d(-10_in, 9_in),
+            wom::Gearbox{
+                driveMotors[2],
+                new wom::TalonFXEncoder(driveMotors[2], 0.0445_m, 6.75),
+                frc::DCMotor::Falcon500(1).WithReduction(6.75)},
+            wom::Gearbox{turnMotors[2],
+                         new wom::CanEncoder(17, 0.0445_m, 4096, 12.8),
                          frc::DCMotor::Falcon500(1).WithReduction(12.8)},
             &backRightCancoder, 4_in / 2},
         wom::SwerveModuleConfig{
             // back right module
-            frc::Translation2d(-10.761_in, -9.455_in),
-            wom::Gearbox{driveMotors[3], new wom::TalonFXEncoder(driveMotors[3], 0.0445_m, 6.75),
-                         frc::DCMotor::Falcon500(1).WithReduction(6.75)},
-            wom::Gearbox{turnMotors[3], new wom::CanEncoder(18, 0.0445_m, 4096, 12.8),
+            frc::Translation2d(-10_in, -9_in),
+            wom::Gearbox{
+                driveMotors[3],
+                new wom::TalonFXEncoder(driveMotors[3], 0.0445_m, 6.75),
+                frc::DCMotor::Falcon500(1).WithReduction(6.75)},
+            wom::Gearbox{turnMotors[3],
+                         new wom::CanEncoder(19, 0.0445_m, 4096, 12.8),
                          frc::DCMotor::Falcon500(1).WithReduction(12.8)},
             &backLeftCancoder, 4_in / 2},
     };
 
     // Setting the PID path and values to be used for SwerveDrive and
     // SwerveModules
-    wom::SwerveModule::angle_pid_conf_t anglePID{
-        "/drivetrain/pid/angle/config", 2_V / 360_deg, 0.0_V / (100_deg * 1_s),
-        0_V / (100_deg / 1_s),          1_deg,         0.5_deg / 2_s};
+    /*wom::SwerveModule::angle_pid_conf_t anglePID{
+        "/drivetrain/pid/angle/config", 90_V / 360_deg, 0.0_V / (100_deg * 1_s),
+        0_V / (100_deg / 1_s)};*/
     wom::SwerveModule::velocity_pid_conf_t velocityPID{
         "/drivetrain/pid/velocity/config",
         //  12_V / 4_mps // webers per metre
     };
-    wom::SwerveDriveConfig::pose_angle_conf_t poseAnglePID{
+    /*wom::SwerveDriveConfig::pose_angle_conf_t poseAnglePID{
         "/drivetrain/pid/pose/angle/config",
-        180_deg / 1_s / 45_deg,
-        wom::SwerveDriveConfig::pose_angle_conf_t::ki_t{0.1},
-        0_deg / 1_deg,
-        10_deg,
-        10_deg / 1_s};
+        0_deg / 1_s / 45_deg,
+        wom::SwerveDriveConfig::pose_angle_conf_t::ki_t{0},
+        0_deg / 1_deg};*/
     wom::SwerveDriveConfig::pose_position_conf_t posePositionPID{
         "/drivetrain/pid/pose/position/config",
-        3_mps / 1_m,
+        0_mps / 1_m,
         wom::SwerveDriveConfig::pose_position_conf_t::ki_t{0.15},
         0_m / 1_m,
-        20_cm,
-        10_cm / 1_s,
-        10_cm};
+        0_cm};
 
     // the config for the whole swerve drive
     wom::SwerveDriveConfig config{"/drivetrain",
-                                  anglePID,
+                                  //anglePID,
                                   velocityPID,
                                   moduleConfigs,  // each module
                                   gyro,
-                                  poseAnglePID,
+                                  //poseAnglePID,
                                   posePositionPID,
                                   60_kg,  // robot mass (estimate rn)
                                   {0.1, 0.1, 0.1},
@@ -129,4 +151,8 @@ struct RobotMap {
     //}
   };
   SwerveBase swerveBase;
+
+  struct SwerveTable {
+    std::shared_ptr<nt::NetworkTable> swerveDriveTable = nt::NetworkTableInstance::GetDefault().GetTable("swerve");
+  }; SwerveTable swerveTable;
 };
